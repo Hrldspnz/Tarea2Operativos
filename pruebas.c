62f7d24c909a9e71534b00b655358e7254faa1c0
hrlds <haroldespinoza823@gmail.com>
2023-08-29 20:17:39 -0600

    pngs

--- pruebas.c
+++ pruebas.c
@@ -2,13 +2,66 @@
 #include <stdlib.h>
 #include "jpeglib.h"
 #include <string.h>
+#include <png.h>
 
 unsigned char *loadGIF(const char *filename, size_t *size, size_t *width, size_t *height) {
     return "s";
 }
 
 unsigned char *loadPNG(const char *filename, size_t *size, size_t *width, size_t *height) {
-    return "s";
+    FILE *file = fopen(filename, "rb");
+    if (!file) {
+        perror("Error al abrir el archivo");
+        return NULL;
+    }
+
+    png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (!png) {
+        fclose(file);
+        perror("Error al crear estructura PNG");
+        return NULL;
+    }
+
+    png_infop info = png_create_info_struct(png);
+    if (!info) {
+        fclose(file);
+        png_destroy_read_struct(&png, NULL, NULL);
+        perror("Error al crear estructura de información PNG");
+        return NULL;
+    }
+
+    png_init_io(png, file);
+    png_read_info(png, info);
+
+    *width = png_get_image_width(png, info);
+    *height = png_get_image_height(png, info);
+    png_byte color_type = png_get_color_type(png, info);
+    png_byte bit_depth = png_get_bit_depth(png, info);
+
+    // Calcular el tamaño total de los datos de la imagen
+    *size = *width * *height * (bit_depth / 8) * png_get_channels(png, info);
+
+    // Reservar memoria para los datos de la imagen
+    unsigned char *imageData = (unsigned char *)malloc(*size);
+    if (!imageData) {
+        fclose(file);
+        png_destroy_read_struct(&png, &info, NULL);
+        perror("Error de asignación de memoria");
+        return NULL;
+    }
+
+    // Leer y cargar los datos de la imagen
+    png_bytep row_pointers[*height];
+    for (size_t y = 0; y < *height; y++) {
+        row_pointers[y] = imageData + y * *width * png_get_channels(png, info) * (bit_depth / 8);
+    }
+    png_read_image(png, row_pointers);
+
+    // Finalizar y limpiar
+    png_destroy_read_struct(&png, &info, NULL);
+    fclose(file);
+
+    return imageData;
 }
 
 unsigned char *loadJPEG(const char *filename, size_t *size, size_t *width, size_t *height) {
